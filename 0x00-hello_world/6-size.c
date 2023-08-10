9666e1deb61763a706bce7cbadd0dbba56023ac3
Driss-Ck <driss.ck10@gmail.com>
2023-08-10 04:36:16 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,6 @@ printf("Size of an int: %lu bytes\n", sizeof(int));
 printf("Size of long int: %lu bytes\n", sizeof(long));
 printf("Size of long long int: %lu bytes\n", sizeof(long long));
 printf("Size of float: %lu bytes\n", sizeof(float));
-printf("Size of double: %lu bytes\n", sizeof(double));
 return (0);
 }
 
