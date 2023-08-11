c220695d47b40ade5f256d19424a784b894507ad
Driss-Ck <driss.ck10@gmail.com>
2023-08-11 03:17:31 -0700

    0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,10 +13,11 @@ int n;
 srand(time(0)); /* Seed the random number generator with the current time*/
 n = rand() % 200 - 100; /* Generate a random number between -100 and 99*/
 if (n > 0)
-printf("%d is positive\n", n);
+printf("%d is positive", n);
 else if (n < 0)
-printf("%d is negative\n", n);
+printf("%d is negative", n);
 else
-printf("%d is zero\n", n);
+printf("%d is zero", n);
+printf("\n");
 return (0);
 }
