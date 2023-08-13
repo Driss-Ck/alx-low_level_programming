1d95a269d72065cae2686962150a604c88e3ce5e
Driss-Ck <driss.ck10@gmail.com>
2023-08-13 03:03:15 -0700

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: Alwyas 0
+ */
+
+int main(void)
+{
+int i;
+for (i = 25; i >= 0; i--)
+{
+putchar('a' + i);
+}
+putchar('\n');
+return (0);
+}
+
