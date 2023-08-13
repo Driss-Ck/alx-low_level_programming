e0f3db5fffe0ac4e2726513f88d6ffcf0f46db61
Driss-Ck <driss.ck10@gmail.com>
2023-08-13 04:37:11 -0700

    102-print_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,31 +5,30 @@
  *
  * Return: Alwyas 0
  */
-
 int main(void)
 {
-int i,j,comma=0;
-for(i=0;i<=99;i++)
+int i, j, comma = 0;
+for (i = 0; i <= 99; i++)
 {
-for(j=i+1;j<=99;j++)
+for (j = i + 1; j <= 99; j++)
 {
-if(comma)
+if (comma)
 {
 putchar(',');
 putchar(' ');
 }
 else
 {
-comma=1;
+comma = 1;
 }
-putchar((i/10)+'0');
-putchar((i%10)+'0');
+putchar((i / 10) + '0');
+putchar((i % 10) + '0');
 putchar(' ');
-putchar((j/10)+'0');
-putchar((j%10)+'0');
+putchar((j / 10) + '0');
+putchar((j % 10) + '0');
 }
 }
 putchar('\n');
-return 0;
+return (0);
 }
 
