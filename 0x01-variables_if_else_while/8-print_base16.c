ebeeaa7b3535f301d142dd149895c1eed9423d6b
Driss-Ck <driss.ck10@gmail.com>
2023-08-13 03:07:22 -0700

    8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: Alwyas 0
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar('0' + i);
+}
+for (i = 0; i < 6; i++)
+{
+putchar('a' + i);
+}
+putchar('\n');
+return (0);
+}
+
